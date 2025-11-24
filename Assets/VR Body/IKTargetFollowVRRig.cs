5813e6ce419b0af1bb1f1d8311511d7c8ef90a4c
parzi <hhhhhsherlock@gmail.com>
2025-11-24 13:22:50 +0100

    tried networking

--- Assets/VR Body/IKTargetFollowVRRig.cs
+++ Assets/VR Body/IKTargetFollowVRRig.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.XR;
 
 [System.Serializable]
 public class VRMap
@@ -7,6 +9,7 @@ public class VRMap
     public Transform ikTarget;
     public Vector3 trackingPositionOffset;
     public Vector3 trackingRotationOffset;
+
     public void Map()
     {
         ikTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
@@ -24,16 +27,51 @@ public class IKTargetFollowVRRig : MonoBehaviour
 
     public Vector3 headBodyPositionOffset;
     public float headBodyYawOffset;
+    private bool assigned = false;
+
+    private void Update()
+    {
+        InputDevice headset = InputDevices.GetDeviceAtXRNode(XRNode.Head);
+
+        // Check if a headset device exists
+        if (!headset.isValid)
+        {
+            Debug.Log("No VR headset detected.");
+            return;
+        }
+
+        // Check if tracking is active
+        if (headset.TryGetFeatureValue(CommonUsages.isTracked, out bool isTracked) && isTracked)
+        {
+            Debug.Log("Headset connected and tracking.");
+            if (!assigned)
+            {
+                Camera cam = Camera.main; 
+                head.vrTarget = cam.transform;
+                assigned = true;
+            }
+        }
+        else
+        {
+            Debug.Log("Headset detected but NOT tracking.");
+        }
+    }
+
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = head.ikTarget.position + headBodyPositionOffset + new Vector3(0f, -0.6f, -0.1f);
-        float yaw = head.vrTarget.eulerAngles.y;
-        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z),turnSmoothness);
+        if (head.vrTarget != null)
+        {
+            transform.position = head.ikTarget.position + headBodyPositionOffset + new Vector3(0f, -0.6f, -0.1f);
+        
+            Debug.Log("this line runs");
+            float yaw = head.vrTarget.eulerAngles.y;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z), turnSmoothness);
 
-        head.Map();
-        leftHand.Map();
-        rightHand.Map();
+            head.Map();
+            leftHand.Map();
+            rightHand.Map();
+        }
     }
 }
